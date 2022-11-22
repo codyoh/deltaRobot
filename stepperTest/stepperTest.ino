dcf3a8ceb8dc40268ef720d00d59130e5838edfa
Cody Oh <codyoferrall@gmail.com>
2022-11-21 22:36:36 -0500

    receiving serial communication in stepperTest

--- stepperTest/stepperTest.ino
+++ stepperTest/stepperTest.ino
@@ -17,26 +17,47 @@ AccelStepper stepper1(motorInterfaceType, stepPin1, directionPin1);
 //create second instance
 AccelStepper stepper2(motorInterfaceType, stepPin2, directionPin2);
 
+int convertAngleToSteps(int angle) {
+  int steps = angle / 1.8;
+  return steps;
+}
+
 void setup() {
+
+  //serial port
+//    Serial.begin(115200);
+  
   // set the maximum speed, acceleration factor,
   // initial speed, and the target position
-  stepper1.setMaxSpeed(1000);
-  stepper1.setSpeed(100);
-  stepper1.setAcceleration(50);
-  stepper1.moveTo(200);
+//  stepper1.setMaxSpeed(1000);
+//  stepper1.setSpeed(100);
+//  stepper1.setAcceleration(50);
+//  stepper1.moveTo(200);
   
   stepper2.setMaxSpeed(1000);
-  stepper2.setSpeed(100);
-  stepper2.setAcceleration(50);
-  stepper2.moveTo(200);
+//  stepper2.setSpeed(100);
+//  stepper2.setAcceleration(50);
+//  stepper2.moveTo(200);
 
 }
 
 void loop() {
+stepper2.setSpeed(100);
+stepper2.runSpeed();
+//  stepper2.run();
+//  delay(500);
 
-  // // Move the motor one step
-  stepper1.run();
-  stepper2.run();
+//  if (Serial.available() > 0) {
+//    String msg = Serial.readString();
+//    int number = msg.toInt();
+//    int steps = convertAngleToSteps(number);
+//    Serial.println(steps);
 
+    // // Move the motor one step
+//    stepper1.moveTo(steps);
+//    stepper1.run();
+  //  stepper2.run();
+//  }
+  
 
 }
