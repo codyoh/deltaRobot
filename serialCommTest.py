183d54133da93e1f98f15e0a176118a0825bb903
Cody Oh <codyoferrall@gmail.com>
2022-11-21 22:39:57 -0500

    added python file for testing send of serial messages

---
+++ serialCommTest.py
@@ -0,0 +1,30 @@
+import time
+import astropy
+import serial
+
+#create a serial object
+arduinoData = serial.Serial('/dev/cu.usbmodem14201', 115200)
+#wait one millisecond for the serial object to finish instantiating
+time.sleep(1)
+
+#create an infinite loop where we will put our code to check the serial port
+while True:
+    command = input("Arduino command: ")
+    arduinoData.write(command.encode('utf-8'))
+
+    if command == 'exit':
+        exit()
+    # #reads when there is no actual data in the serial
+    while(arduinoData.inWaiting()==0):
+        pass
+
+    #arduino reads the serial line
+    dataPacket = arduinoData.readline()
+    #convert that byte data to a utf-8 string
+    dataPacket = str(dataPacket, 'utf-8')
+    #strip off the extra lines from \r\n at end of string
+    dataPacket = dataPacket.strip('\r\n')
+    #print to terminal
+    print(dataPacket)
+
+
